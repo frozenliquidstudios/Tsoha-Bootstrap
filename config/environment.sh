24b5dee2ac7c4a19e14d87a8223f416d300447d9
Jere P Hupanen <jhupanen@svm-19.cs.helsinki.fi>
2015-03-15 18:23:49 +0200

    Sovellus alustettu ja versionahallinta lisätty.

--- config/environment.sh
+++ config/environment.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # korvaa tämä omalla käyttäjätunnuksellasi
-USERNAME="kalleilv"
+USERNAME="jhupanen"
 # korvaa tämä haluamallasi kansion nimellä
-PROJECT_FOLDER="tsoha"
+PROJECT_FOLDER="clipBase"
 # sovelluksesi tulee sijaitsemaan osoitteessa USERNAME.users.cs.helsinki.fi/PROJECT_FOLDER
